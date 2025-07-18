1e47bd24e594a9a3f125678eb80da88621e1bc90
stellarshenson <konrad.jelen@gmail.com>
2025-07-18 02:48:43 +0200

    updated startup script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ CURRENT_DIR=`dirname $CURRENT_FILE`
 cd $CURRENT_DIR
 
 # Run the command for when GPU is not available
-if [[ -f './compose-override.yml' ]]; then
+if [ -f './compose-override.yml' ]; then
     docker compose --env-file .env \
 	-f compose.yml \
 	-f compose_override.yml \
