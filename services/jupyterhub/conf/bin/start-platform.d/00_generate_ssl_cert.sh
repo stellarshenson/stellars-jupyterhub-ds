b1ead8905e3b9dd1d83a66ec78fdf08735945140
stellarshenson <konrad.jelen@gmail.com>
2026-01-05 15:09:32 +0100

    feat: add CERTIFICATE_DOMAIN_NAME env variable for cert generation
    - Add CERTIFICATE_DOMAIN_NAME to compose.yml (defaults to localhost)
    - Update 00_generate_ssl_cert.sh to use env variable
    - Allows custom domain names for self-signed certificates

--- services/jupyterhub/conf/bin/start-platform.d/00_generate_ssl_cert.sh
+++ services/jupyterhub/conf/bin/start-platform.d/00_generate_ssl_cert.sh
@@ -6,8 +6,9 @@
 # generate ssl keys if don't exist yet (happens first time the script is run)
 # skip this step if no certificate dir
 CERTS_DIR="/mnt/certs"
+DOMAIN_NAME="${CERTIFICATE_DOMAIN_NAME:-localhost}"
 if [[ -z $(find $CERTS_DIR -name '*.crt') ]]; then
-	/mkcert.sh "$CERTS_DIR" "localhost" "server" # parsms: certs_dir, common_name, file_prefix
+	/mkcert.sh "$CERTS_DIR" "$DOMAIN_NAME" "server" # params: certs_dir, common_name, file_prefix
 fi
 
 
