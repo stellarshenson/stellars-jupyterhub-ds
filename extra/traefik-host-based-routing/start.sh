3a7d697834df6f290203d15bbdf0b618811e90ef
stellarshenson <konrad.jelen@gmail.com>
2026-01-06 23:00:16 +0100

    feat: add --refresh flag to traefik template start.sh

--- extra/traefik-host-based-routing/start.sh
+++ extra/traefik-host-based-routing/start.sh
@@ -7,11 +7,31 @@ cd "$(dirname "$0")"
 
 REPO_URL="https://github.com/stellarshenson/stellars-jupyterhub-ds.git"
 REPO_DIR="stellars-jupyterhub-ds"
+REFRESH=false
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --refresh)
+            REFRESH=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Usage: $0 [--refresh]"
+            exit 1
+            ;;
+    esac
+done
 
 if [ -d "$REPO_DIR" ]; then
-    echo "Pulling latest $REPO_DIR..."
-    git -C "$REPO_DIR" fetch origin main
-    git -C "$REPO_DIR" checkout -B main origin/main
+    if [ "$REFRESH" = true ]; then
+        echo "Refreshing $REPO_DIR..."
+        git -C "$REPO_DIR" fetch origin main
+        git -C "$REPO_DIR" checkout -B main origin/main
+    else
+        echo "Using existing $REPO_DIR (use --refresh to update)"
+    fi
 else
     echo "Cloning $REPO_DIR..."
     git clone "$REPO_URL"
